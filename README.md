bf0e48d6dca9445608c20bdae4a1bb08816139bf
Alexander Castilla <alexdead2118@gmail.com>
2022-01-23 20:38:45 -0500

    fixed

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # BSALE FrontEnd
 
 ## Buscador conectado a la apirest de Bsale
+Visista el demo en: [FRONTEND BSALE DEMO](https://peaceful-brahmagupta-aeb221.netlify.app/).
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,20 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: "./js/app.js",
   output: {
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
   mode: "production",
   plugins: [
     new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+  }),
   ],
   module: {
     rules: [
